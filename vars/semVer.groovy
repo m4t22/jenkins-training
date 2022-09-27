a76748f6d3fe043da8e2174fbf391472322950ce
Mateusz Ziarko <mat.ziarko@gmail.com>
2022-09-27 16:22:48 +0200

    feat: set upstream

--- vars/semVer.groovy
+++ vars/semVer.groovy
@@ -2,5 +2,6 @@ def call() {
     git credentialsId: 'm4t22-creds', branch: 'main', url: 'git@github.com:m4t22/jenkins-training.git'
     sh './gradlew clean'
     sh './gradlew releaseVersion'
+    sh 'git push --set-upstream origin main'
     sh 'git push --follow-tags'
 }
