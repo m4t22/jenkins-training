67f7d8783f69a54d300dd2c82592429a71004434
Mateusz Ziarko <mat.ziarko@gmail.com>
2022-09-27 16:13:18 +0200

    feat: test4

--- vars/semVer.groovy
+++ vars/semVer.groovy
@@ -1,5 +1,6 @@
 def call() {
     git credentialsId: 'm4t22-creds', branch: 'main', url: 'git@github.com:m4t22/jenkins-training.git'
+    sh './gradlew clean'
     sh './gradlew releaseVersion'
     sh 'git push --follow-tags'
 }
