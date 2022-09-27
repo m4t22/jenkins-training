1cbce34ddc049ab46cfb4832bd02dbb884ad7041
Mateusz Ziarko <mat.ziarko@gmail.com>
2022-09-27 16:30:11 +0200

    fix: shorten the semVer call

--- vars/semVer.groovy
+++ vars/semVer.groovy
@@ -1,7 +1,7 @@
 def call() {
     git credentialsId: 'm4t22-creds', branch: 'main', url: 'git@github.com:m4t22/jenkins-training.git'
-    sh './gradlew clean'
-    sh './gradlew releaseVersion'
-    sh 'git push --set-upstream origin main'
-    sh 'git push --follow-tags'
+    sh '''
+        ./gradlew releaseVersion
+        git push --set-upstream origin main --follow-tags
+    '''
 }
